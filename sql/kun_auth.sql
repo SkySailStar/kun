618f52d6329aec6301bb1a823537857c976ab9f6
sail <yifansailing@163.com>
2024-05-18 18:31:51 +0800

    初始搭建

--- sql/kun_auth.sql
+++ sql/kun_auth.sql
@@ -1,8 +1,6 @@
 SET NAMES utf8mb4;
 
--- ----------------------------
 -- 1、部门表
--- ----------------------------
 drop table if exists sys_dept;
 create table sys_dept (
                           dept_id           bigint(20)      not null                   comment '部门id',
@@ -14,25 +12,20 @@ create table sys_dept (
                           phone             varchar(11)     default null               comment '联系电话',
                           email             varchar(50)     default null               comment '邮箱',
                           status            char(1)         default '0'                comment '部门状态（0正常 1停用）',
-                          del_flag          char(1)         default '0'                comment '删除标志（0代表存在 2代表删除）',
                           create_by         varchar(64)     default ''                 comment '创建者',
                           create_time 	    datetime                                   comment '创建时间',
                           update_by         varchar(64)     default ''                 comment '更新者',
                           update_time       datetime                                   comment '更新时间',
                           primary key (dept_id)
-) engine=innodb =200 comment = '部门表';
+) engine=innodb comment = '部门表';
 
--- ----------------------------
 -- 初始化-部门表数据
--- ----------------------------
 insert into sys_dept values(100,  0,   '0',          '天航星科技',   0, '琨', '15888888888', '123456@qq.com', '0', '0', 'admin', sysdate(), '', null);
 insert into sys_dept values(101,  100, '0,100',      '重庆总公司', 1, '琨', '15888888888', '123456@qq.com', '0', '0', 'admin', sysdate(), '', null);
 insert into sys_dept values(103,  101, '0,100,101',  '研发部门',   1, '琨', '15888888888', '123456@qq.com', '0', '0', 'admin', sysdate(), '', null);
 
 
--- ----------------------------
 -- 2、用户信息表
--- ----------------------------
 drop table if exists sys_user;
 create table sys_user (
                           user_id           bigint(20)      not null                   comment '用户ID',
@@ -41,12 +34,11 @@ create table sys_user (
                           nick_name         varchar(30)     not null                   comment '用户昵称',
                           user_type         varchar(2)      default '00'               comment '用户类型（00系统用户）',
                           email             varchar(50)     default ''                 comment '用户邮箱',
-                          phonenumber       varchar(11)     default ''                 comment '手机号码',
+                          phone             varchar(11)     default ''                 comment '联系电话',
                           sex               char(1)         default '0'                comment '用户性别（0男 1女 2未知）',
                           avatar            varchar(100)    default ''                 comment '头像地址',
                           password          varchar(100)    default ''                 comment '密码',
                           status            char(1)         default '0'                comment '帐号状态（0正常 1停用）',
-                          del_flag          char(1)         default '0'                comment '删除标志（0代表存在 2代表删除）',
                           login_ip          varchar(128)    default ''                 comment '最后登录IP',
                           login_date        datetime                                   comment '最后登录时间',
                           create_by         varchar(64)     default ''                 comment '创建者',
@@ -57,15 +49,11 @@ create table sys_user (
                           primary key (user_id)
 ) engine=innodb comment = '用户信息表';
 
--- ----------------------------
 -- 初始化-用户信息表数据
--- ----------------------------
 insert into sys_user values(1,  103, 'admin', '管理员', '00', '123456@163.com', '15888888888', '1', '', '$2a$10$7JB720yubVSZvUI0rEqK/.VqGOZTH.ulu33dHOiBE8ByOhJIrdAu2', '0', '0', '127.0.0.1', sysdate(), 'admin', sysdate(), '', null, '管理员');
 
 
--- ----------------------------
 -- 3、岗位信息表
--- ----------------------------
 drop table if exists sys_post;
 create table sys_post
 (
@@ -82,18 +70,14 @@ create table sys_post
     primary key (post_id)
 ) engine=innodb comment = '岗位信息表';
 
--- ----------------------------
 -- 初始化-岗位信息表数据
--- ----------------------------
 insert into sys_post values(1, 'ceo',  '董事长',    1, '0', 'admin', sysdate(), '', null, '');
 insert into sys_post values(2, 'se',   '项目经理',  2, '0', 'admin', sysdate(), '', null, '');
 insert into sys_post values(3, 'hr',   '人力资源',  3, '0', 'admin', sysdate(), '', null, '');
 insert into sys_post values(4, 'user', '普通员工',  4, '0', 'admin', sysdate(), '', null, '');
 
 
--- ----------------------------
 -- 4、角色信息表
--- ----------------------------
 drop table if exists sys_role;
 create table sys_role (
                           role_id              bigint(20)      not null     comment '角色ID',
@@ -104,7 +88,6 @@ create table sys_role (
                           menu_check_strictly  tinyint(1)      default 1                  comment '菜单树选择项是否关联显示',
                           dept_check_strictly  tinyint(1)      default 1                  comment '部门树选择项是否关联显示',
                           status               char(1)         not null                   comment '角色状态（0正常 1停用）',
-                          del_flag             char(1)         default '0'                comment '删除标志（0代表存在 2代表删除）',
                           create_by            varchar(64)     default ''                 comment '创建者',
                           create_time          datetime                                   comment '创建时间',
                           update_by            varchar(64)     default ''                 comment '更新者',
@@ -113,16 +96,12 @@ create table sys_role (
                           primary key (role_id)
 ) engine=innodb =100 comment = '角色信息表';
 
--- ----------------------------
 -- 初始化-角色信息表数据
--- ----------------------------
 insert into sys_role values('1', '超级管理员',  'admin',  1, 1, 1, 1, '0', '0', 'admin', sysdate(), '', null, '超级管理员');
 insert into sys_role values('2', '普通角色',    'common', 2, 2, 1, 1, '0', '0', 'admin', sysdate(), '', null, '普通角色');
 
 
--- ----------------------------
 -- 5、菜单权限表
--- ----------------------------
 drop table if exists sys_menu;
 create table sys_menu (
                           menu_id           bigint(20)      not null     comment '菜单ID',
@@ -147,9 +126,7 @@ create table sys_menu (
                           primary key (menu_id)
 ) engine=innodb =2000 comment = '菜单权限表';
 
--- ----------------------------
 -- 初始化-菜单信息表数据
--- ----------------------------
 -- 一级菜单
 insert into sys_menu values('1', '系统管理', '0', '1', 'system',           null, '', 1, 0, 'M', '0', '0', '', 'system',   'admin', sysdate(), '', null, '系统管理目录');
 insert into sys_menu values('2', '系统监控', '0', '2', 'monitor',          null, '', 1, 0, 'M', '0', '0', '', 'monitor',  'admin', sysdate(), '', null, '系统监控目录');
@@ -252,9 +229,7 @@ insert into sys_menu values('1059', '预览代码', '115', '4', '#', '', '', 1, 
 insert into sys_menu values('1060', '生成代码', '115', '5', '#', '', '', 1, 0, 'F', '0', '0', 'tool:gen:code',              '#', 'admin', sysdate(), '', null, '');
 
 
--- ----------------------------
 -- 6、用户和角色关联表  用户N-1角色
--- ----------------------------
 drop table if exists sys_user_role;
 create table sys_user_role (
                                user_id   bigint(20) not null comment '用户ID',
@@ -262,16 +237,12 @@ create table sys_user_role (
                                primary key(user_id, role_id)
 ) engine=innodb comment = '用户和角色关联表';
 
--- ----------------------------
 -- 初始化-用户和角色关联表数据
--- ----------------------------
 insert into sys_user_role values ('1', '1');
 insert into sys_user_role values ('2', '2');
 
 
--- ----------------------------
 -- 7、角色和菜单关联表  角色1-N菜单
--- ----------------------------
 drop table if exists sys_role_menu;
 create table sys_role_menu (
                                role_id   bigint(20) not null comment '角色ID',
@@ -279,9 +250,7 @@ create table sys_role_menu (
                                primary key(role_id, menu_id)
 ) engine=innodb comment = '角色和菜单关联表';
 
--- ----------------------------
 -- 初始化-角色和菜单关联表数据
--- ----------------------------
 insert into sys_role_menu values ('2', '1');
 insert into sys_role_menu values ('2', '2');
 insert into sys_role_menu values ('2', '3');
